3a6521cb4f3b368f4eaa7f6e9f1a2f63ff7efd08
ROHIT DHAKAD <rohitdhakadrj28@gmail.com>
2024-02-18 15:49:46 +0530

    modified:   Heap_sort.cpp

--- Heap_sort.cpp
+++ Heap_sort.cpp
@@ -24,39 +24,25 @@ void buildheap(int arr[], int n, int current_i)
 }
 void heapcheck(int arr[], int n)
 {
-    // int current_i = n / 2;
-    for (int i = n/2-1; i >=0; i--)
+    int current_i = n / 2;
+    for (int i = n/2; i >=0; i--)
     {
-        buildheap(arr, n, i);
+        buildheap(arr, n, current_i);
     }
 }
 void heapsort(int arr[], int n)
 {
-    // for (int i = n / 2; i >= 0; i--)
-    // {
-    //     buildheap(arr, n, i);
-    // }
-    // cout << "Heap array: \n";
-    // for (int i = 0; i < n; i++)
-    //     cout << " " << arr[i];
-    // cout << endl;
     for (int i = n - 1; i >= 0; i--)
     {
         swap(arr[0], arr[i]);
-        // (arr, (i)-1);
-
-        // for (int j = i / 2 - 1; j >=0; j--)
-        // {
-        //      buildheap(arr,n, j);
-        // }
-        buildheap(arr,i+1,1);
+        heapcheck(arr,(i-1));
     }
 }
 int main()
 {
     int n = 7;
 
-    int arr[7] = {2, 3, 6, 8, 33, 7, 1};
+    int arr[7] = {2, 3, 6, 8, 33, 7, 5};
     for (int i = 0; i < n; i++)
         cout << " " << arr[i] << " ";
     cout << endl;
